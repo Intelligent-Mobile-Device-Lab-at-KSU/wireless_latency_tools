421294eedd18c12e6595c3fbc1c8c531af4d8c93
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2022-02-26 23:49:48 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,16 @@
-hi
+import socket
+import time
+
+serverAddressPort   = ("127.0.0.1", 4400)
+bufferSize          = 1
+
+# Create a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+# Send to server using created UDP socket
+while(True):
+    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    t = time.time()
+    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+    elapsed = time.time()-t
+    print("From %s, elapsed: %d" % (msgFromServer[0].decode(),elapsed))
