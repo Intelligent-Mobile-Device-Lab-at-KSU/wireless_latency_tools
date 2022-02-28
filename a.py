9fa35f5a609841eb27d6fdc8241022a1ff7ee44f
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2022-02-28 12:44:48 -0500

    Update a.py

--- a.py
+++ a.py
@@ -1,19 +1,32 @@
 import socket
 import time
 
-msgFromClient       = "0"
-bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 14400)
-bufferSize          = 1024
+message = "0"
+bytesToSend = message.encode()
+localIP     = "127.0.0.1"
+localPort   = 14400
+bufferSize  = 1
 
-# Create a UDP socket at client side
-UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+# Create a datagram socket
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-# Send to server using created UDP socket
+# Bind to address and ip
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+message = bytesAddressPair[0]
+address = bytesAddressPair[1]
+clientMsg = "Message from Client:{}".format(message)
+clientIP  = "Client IP Address:{}".format(address)
+print(clientMsg)
+print(clientIP)
 while(True):
-    print("sending...")
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+    print("Sent pkt")
+    UDPServerSocket.sendto(bytesToSend, address)
     t = time.time()
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    elapsed = time.time()-t
-    print("From %s, elapsed: %d" % (msgFromServer[0].decode(),elapsed))
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    elaps = (time.time() - t)*1000
+    print("Rx: %s" % (bytesAddressPair[0]))
+    time.sleep(.5)
