1f697b792e8f44339a96d70afd7413549c10b13f
scrappylabs <57416600+scrappylabs@users.noreply.github.com>
2022-03-17 15:26:33 -0400

    Update test_1_0.py

--- test_1_0.py
+++ test_1_0.py
@@ -1,1 +1,14 @@
-a
+# echo-client.py
+import socket
+import time
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 14400  # The port used by the server
+
+tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_client_socket.connect((HOST, PORT))
+while 1:
+    tcp_client_socket.sendall(b"0")
+    time.sleep(.5)
+    tcp_client_socket.sendall(b"1")
+    time.sleep(.5)
